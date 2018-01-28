aaf429c6c0746f82c35414e4a1f341a11e0bf9bb
Sam Clark <samuel.clark@stonybrook.edu>
2018-01-28 17:26:43 -0500

    Peep on Heroku

--- src/api-config.js
+++ src/api-config.js
@@ -2,13 +2,12 @@ let serverUrl, wsUrl;
 
 const hostname = window && window.location && window.location.hostname;
 
-if(hostname === 'peep-chat.herokuapp.com') {
-	serverUrl = 'https://peep-chat.herokuapp.com/api';
-	wsUrl = 'wss://peep-chat.herokuapp.com/api/ws';
-
-} else {
+if(hostname === 'localhost') {
 	serverUrl = 'http://localhost:8080/api';
 	wsUrl = 'ws://localhost:8080/api/ws';
+} else {
+	serverUrl = 'https://peep-chat.herokuapp.com/api';
+	wsUrl = 'wss://peep-chat.herokuapp.com/api/ws';
 }
 
 module.exports = { serverUrl, wsUrl };
