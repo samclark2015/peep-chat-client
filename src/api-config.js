a3c23d1854c95c7f413077d402ef9c6f76fb9f30
Sam Clark <samuel.clark@stonybrook.edu>
2018-01-28 20:27:23 -0500

    Runtime vars for API endpoints

--- src/api-config.js
+++ src/api-config.js
@@ -8,8 +8,8 @@ console.log(env);
 const hostname = window && window.location && window.location.hostname;
 
 if(env.REACT_APP_SERVER_URL && env.REACT_APP_WS_URL) {
-	serverUrl = env.SERVER_URL;
-	wsUrl = env.WS_URL;
+	serverUrl = env.REACT_APP_SERVER_URL;
+	wsUrl = env.REACT_APP_WS_URL;
 } else {
 	serverUrl = 'http://localhost:8080/api';
 	wsUrl = 'ws://localhost:8080/api/ws';
