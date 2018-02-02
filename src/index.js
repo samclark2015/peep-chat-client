96afc6acde74a1ceb0ed87f5b455b53035fa2174
Sam Clark <samuel.clark@stonybrook.edu>
2018-02-01 23:11:32 -0500

    Removed caching service worker during dev

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
